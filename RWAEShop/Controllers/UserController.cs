e27096c7947a8f44d4e0a7fcc5ce8204dc71a968
IvanPetar <ivanpetarsolic1@gmail.com>
2025-06-03 04:51:05 +0200

    Implemented Uerr Controller and Security

---
+++ RWAEShop/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RWAEshopDAL.Security;
+
+namespace RWAEShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult GetToken()
+        {
+
+
+            try
+            {
+
+                var secureKey = _configuration["JWT:SecureKey"];
+                var serializedToken = JwtTokenProvider.CreateToken(secureKey, 10);
+
+                return Ok(serializedToken);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
--- RWAEShop/Program.cs
+++ RWAEShop/Program.cs
@@ -4,6 +4,10 @@ using RWAEShop.Profiles;
 using RWAEshopDAL.Repositories;
 using RWAEshopDAL.Services;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +21,58 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1",
+        new OpenApiInfo { Title = "RWAEShop", Version = "v1" });
+
+    option.AddSecurityDefinition("Bearer",
+        new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter valid JWT",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+
+    option.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new List<string>()
+            }
+        });
+});
+
+
+// Configure JWT security services
+var secureKey = builder.Configuration["JWT:SecureKey"];
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o => {
+        var Key = Encoding.UTF8.GetBytes(secureKey);
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            IssuerSigningKey = new SymmetricSecurityKey(Key)
+        };
+    });
+
+
 
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
@@ -35,8 +91,6 @@ builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddDbContext<EshopContext>();
 
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -46,6 +100,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
